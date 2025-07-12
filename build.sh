274ce9a322ab4a9e419ff5ed89c4d4336f3fd17d
HafizTayyabNasir <hafiztayab1000@gmail.com>
2025-07-13 03:42:48 +0500

    FINAL FIX: Set specific python version and clean dependencies

--- build.sh
+++ build.sh
@@ -1,21 +1,5 @@
-#!/bin/bash
-
-# This script tells Vercel how to build your Django project.
-
-# Exit immediately if a command exits with a non-zero status.
 set -o errexit
 
-# The Vercel Python builder automatically creates and activates a
-# virtual environment, so we can now use 'pip' and 'python' directly.
-
-# Upgrade pip
-pip install --upgrade pip
-
-# Install all the Python packages
-pip install -r requirements.txt
-
-# Collect all of Django's static files
-python manage.py collectstatic --noinput
-
-# Apply any pending database migrations
-python manage.py migrate
+python3.11 -m pip install -r requirements.txt
+python3.11 manage.py collectstatic --noinput
+python3.11 manage.py migrate
