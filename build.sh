3c213421b9e40bdd236b52a507c276ae2b6c9d0a
HafizTayyabNasir <hafiztayab1000@gmail.com>
2025-07-15 02:19:46 +0500

    Update project with latest changes and fixes

--- build.sh
+++ build.sh
@@ -1,3 +1,16 @@
+#!/bin/bash
+
+# Exit immediately if a command exits with a non-zero status.
 set -o errexit
+
+echo "--- Building Elvion Project ---"
+
+# --- Installation ---
+echo "--- Installing dependencies ---"
 pip install -r requirements.txt
+
+# --- Collect Static Files ONLY ---
+echo "--- Collecting static files ---"
 python manage.py collectstatic --noinput --clear
+
+echo "--- Build Complete ---"
