b97c01bdc4258a9ccd135aaf33db94cf9db151bd
HafizTayyabNasir <hafiztayab1000@gmail.com>
2025-07-15 02:10:48 +0500

    FINAL FIX: Use vercel-build script in package.json for migrations

--- build.sh
+++ build.sh
@@ -1,16 +1,3 @@
-#!/bin/bash
-
-# Exit immediately if a command exits with a non-zero status.
 set -o errexit
-
-echo "--- Building Elvion Project ---"
-
-# --- Installation ---
-echo "--- Installing dependencies ---"
 pip install -r requirements.txt
-
-# --- Collect Static Files ONLY ---
-echo "--- Collecting static files ---"
 python manage.py collectstatic --noinput --clear
-
-echo "--- Build Complete ---"
