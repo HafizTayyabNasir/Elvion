0590d7b0a094a946cff17d4b15e4a92af687aa30
HafizTayyabNasir <hafiztayab1000@gmail.com>
2025-07-15 14:48:35 +0500

    FORCE PUSH: Final attempt to deploy admin creation URL

--- elvion_project/urls.py
+++ elvion_project/urls.py
@@ -1,12 +1,11 @@
 # elvion_project/urls.py
 from django.contrib import admin
 from django.urls import path, include
-# Import the correctly named view
-from website.views import create_superuser_secret_view 
+from website.views import create_superuser_secret_view
 
 urlpatterns = [
-    # Use the correctly named view
-    path('create-my-admin-now-and-forever-12345/', create_superuser_secret_view, name='create_admin'),
+    # THIS LINE MUST BE HERE
+    path('make-me-admin-now-and-forever-12345/', create_superuser_secret_view, name='create_admin'),
     
     path('admin/', admin.site.urls),
     path('', include('website.urls')),
